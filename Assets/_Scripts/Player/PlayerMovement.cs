83700d6b6701203ce48209bc58f54dfbd4a8e3fe
Bennett Smrdel <fireknights318@gmail.com>
2023-04-22 11:09:31 -0500

    feat: Can move player

--- Assets/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private void Start()
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed = 7f;
+    private float horizontal, vertical;
+
+
+    private void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        transform.position += new Vector3(horizontal, vertical, 0) * moveSpeed * Time.deltaTime;
     }
 }
